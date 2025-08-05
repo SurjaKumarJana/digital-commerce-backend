84902d134a09c8562cf95ba3d3b2db2311c0d1a9
Surja Kumar Jana <learn.surja01@gmail.com>
2025-08-05 12:05:44 +0530

    ecommerce-backend-service : initializing project

---
+++ src/main/java/org/surja/digital_commerce_backend/DigitalCommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package org.surja.digital_commerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalCommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalCommerceBackendApplication.class, args);
+	}
+
+}
