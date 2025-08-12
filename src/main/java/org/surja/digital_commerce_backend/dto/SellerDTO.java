13a3af23e8d94669deb723d2ff814ed3ec10e4bf
Surja Kumar Jana <learn.surja01@gmail.com>
2025-08-12 22:54:52 +0530

    dto : added  validation on dto

--- src/main/java/org/surja/digital_commerce_backend/dto/SellerDTO.java
+++ src/main/java/org/surja/digital_commerce_backend/dto/SellerDTO.java
@@ -1,5 +1,8 @@
 package org.surja.digital_commerce_backend.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.surja.digital_commerce_backend.entity.User;
 
@@ -10,8 +13,20 @@ import org.surja.digital_commerce_backend.entity.User;
 @AllArgsConstructor
 public class SellerDTO {
     private Long id;
+    // in order to add some extra validation while taking input parameters
+    // we use thise annotation
+    // and we need a extra dependency for this :
+    //      <dependency>
+    //			<groupId>org.springframework.boot</groupId>
+    //			<artifactId>spring-boot-starter-validation</artifactId>
+    //		</dependency>
+    @NotNull
+    @Size(min =2)
     private String name;
+    @NotNull
+    @Email
     private String email;
+    @NotNull
     private Long companyId;
 
 
